329fe65bed44e0b8a7540b9d73b4c4a90ac0c615
Stefan Ossenbrink <so634597@fh-muenster.de>
2016-10-19 19:19:22 +0200

    Initial Commit

---
+++ src/main/java/de/nog/WRBObserver.java
@@ -0,0 +1,114 @@
+package de.nog;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.antlr.v4.runtime.ParserRuleContext;
+import org.antlr.v4.runtime.Token;
+import org.antlr.v4.runtime.misc.NotNull;
+import org.antlr.v4.runtime.tree.*;
+
+import de.nog.antlr.WRBLexer;
+import de.nog.antlr.WRBParser;
+import de.nog.antlr.WRBParser.AdditionContext;
+import de.nog.antlr.WRBParser.ExpressionContext;
+//import de.nog.antlr.WRBParser.AssignContext;
+//import de.nog.antlr.WRBParser.ExpressionContext;
+import de.nog.antlr.WRBParser.StatementContext;
+import de.nog.antlr.WRBParserBaseListener;
+
+public class WRBObserver extends WRBParserBaseListener {
+	public WRBObserver(WRBScript wrbScript) {
+		this.script = wrbScript;
+		if (null == script)
+			throw new IllegalArgumentException("script is null");
+	}
+
+	protected Map<ParseTree, Double> values = new HashMap<ParseTree, Double>();
+	protected WRBScript script;
+	protected double lastValue;
+
+	/*
+	 * @Override public void exitAssign(@NotNull WRBParser.AssignContext ctx) {
+	 * String id = ctx.ID().getSymbol().getText(); // double value =
+	 * getValue(ctx.expression()); // script.setVariable(id, value); // //
+	 * setValue(ctx, value); }
+	 */
+	/*
+	 * @Override public void enterExpression(ExpressionContext ctx) {
+	 * System.out.println("Enter Expression"); }
+	 */
+	@Override
+	public void enterStatement(StatementContext ctx) {
+		// System.out.println("enter statement. children:" + ctx.children);
+
+	}
+
+	@Override
+	public void exitStatement(StatementContext ctx) {
+		// System.out.println("exit statement. children: " + ctx.children);
+
+		lastValue = getValue(ctx.expression());
+		setValue(ctx, lastValue);
+		System.out.println("Statement returns " + lastValue);
+	}
+
+	@Override
+	public void exitExpression(ExpressionContext ctx) {
+		setValue(ctx, getValue(ctx.addition()));
+	}
+
+	@Override
+	public void exitAddition(AdditionContext ctx) {
+		double summe = 0;
+		if (ctx.operator.get(0).getType() == WRBParser.ADD) {
+			summe = Double.parseDouble(ctx.constant(0).INTEGER().getText())
+					+ Double.parseDouble(ctx.constant(1).INTEGER().getText());
+		} else {
+			summe = Double.parseDouble(ctx.constant(0).INTEGER().getText())
+					- Double.parseDouble(ctx.constant(1).INTEGER().getText());
+		}
+		setValue(ctx, summe);
+	}
+
+	private void setValue(ParseTree ctx, double value) {
+		values.put(ctx, value);
+	}
+
+	@Override
+	public void enterEveryRule(@NotNull ParserRuleContext ctx) {
+		System.out.println("enter rule:" + ctx.getClass().toString().substring(ctx.getClass().toString().indexOf('$')));
+	}
+
+	@Override
+	public void exitEveryRule(@NotNull ParserRuleContext ctx) {
+		System.out.println("exit rule:" + ctx.getClass().toString().substring(ctx.getClass().toString().indexOf('$')));
+	}
+	/*
+	 * @Override public void exitExpression(@NotNull WRBParser.ExpressionContext
+	 * ctx) { int k = 0; double value = getValue(ctx.term(k)); ParseTree node =
+	 * ctx.term(++k); while (null != node) { Token op = ctx.operator.get(k - 1);
+	 * if (WRBLexer.ADD == op.getType()) { value += getValue(node); } else {
+	 * value -= getValue(node); } } setValue(ctx, value); }
+	 */
+	/*
+	 * @Override public void exitDotop(@NotNull WRBParser.DotopContext ctx){
+	 * 
+	 * }
+	 * 
+	 * private void setValue(ExpressionContext ctx, double value) {
+	 * values.replace(ctx, value); }
+	 */
+
+	private double getValue(ParseTree node) {
+		if (values.containsKey(node)) {
+			return values.get(node);
+		}
+		throw new IllegalArgumentException();
+	}
+
+	public double getLastValue() {
+		return lastValue;
+	}
+
+}
---
+++ src/main/java/de/nog/WRBScript.java
@@ -0,0 +1,66 @@
+package de.nog;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Map;
+import java.util.TreeMap;
+
+import org.antlr.v4.runtime.ANTLRInputStream;
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.tree.ParseTree;
+import org.antlr.v4.runtime.tree.ParseTreeWalker;
+
+import de.nog.antlr.WRBLexer;
+import de.nog.antlr.WRBParser;
+
+//import main.antlr.WRBParser;
+
+//von mir erstellt damit der compiler happy ist
+public class WRBScript implements Script {
+	Map<String, Double> variables;
+	Map<String, Function> functions;
+
+	public WRBScript() {
+		super();
+		variables = new TreeMap<String, Double>();
+		functions = new TreeMap<String, Function>();
+	}
+
+	public Function getFunction(String name) throws IllegalArgumentException {
+		if (functions.containsKey(name))
+			return functions.get(name);
+		throw new IllegalArgumentException();
+	}
+
+	public double getVariable(String name) throws IllegalArgumentException {
+		if (variables.containsKey(name))
+			return variables.get(name);
+		throw new IllegalArgumentException();
+	}
+
+	public void setVariable(String name, double value) {
+		variables.replace(name, value);
+
+	}
+
+	public double parse(String definition) throws IllegalArgumentException {
+		CharStream stream = new ANTLRInputStream(definition);
+		WRBLexer lexi = new WRBLexer(stream);
+		CommonTokenStream tokens = new CommonTokenStream(lexi);
+		WRBParser parsi = new WRBParser(tokens);
+		WRBObserver obs = new WRBObserver(this);
+		parsi.setBuildParseTree(true);
+		ParseTree tree = parsi.statement();
+		ParseTreeWalker.DEFAULT.walk(obs, tree);
+
+		return obs.getLastValue();
+
+	}
+
+	public double parse(InputStream defStream) throws IllegalArgumentException, IOException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
