e56c861e24441ae27d837fa5b4b459333481f9ad
stefan <stefan@stefan-linux-pc>
2016-12-06 18:16:13 +0100

    added cpp-library and tested loading of it

---
+++ src/test/java/de/lab4inf/wrb/Cpp_LibraryTest.java
@@ -0,0 +1,14 @@
+package de.lab4inf.wrb;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Cpp_LibraryTest {
+
+	@Test
+	public void test() {
+		System.load("/home/stefan/git/wulffrunsbeta/src/main/libHPK_Praktikum4_Cpp");
+	}
+
+}
