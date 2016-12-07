fa8dd3fadb2292cd533530c9a15644a1d0cbd370
Stefan Ossenbrink <lipownski@googlemail.com>
2016-12-07 17:24:10 +0100

    Added test, made lib silent

--- src/test/java/de/lab4inf/wrb/Cpp_LibraryTest.java
+++ src/test/java/de/lab4inf/wrb/Cpp_LibraryTest.java
@@ -49,8 +49,58 @@ public class Cpp_LibraryTest {
 		Integrator i = new Integrator();
 		for (double x = 1; x < 10; x += 1) {
 
-			double y = i.integrate(wrbs.getFunction("x_quadrat"),0, x);
+			double y = i.integrate(wrbs.getFunction("x_quadrat"), 0, x);
 			assertEquals(x * x * x / (double) 3, y, EPS);
+		}
+	}
+
+	@Test
+	public void drawMeAFuckingTable() {
+		Function sq = new Function() {
+			@Override
+			public double eval(double... args) {
+				return args[0] * args[0];
+			}
+		};
+		Function e = new Function() {
+			@Override
+			public double eval(double... args) {
+				return  Math.pow(Math.E,args[0]);
+			}
+		};
+		
+		Function sin = new Function() {
+			@Override
+			public double eval(double... args) {
+				return Math.sin(args[0]);
+			}
+		};
+		
+		
+		
+		System.out.println("x^2");
+		drawTable(sq);
+		System.out.println("e");
+		drawTable(e);
+		System.out.println("sin");
+		drawTable(sin,0,Math.PI,Math.PI/8.0);
+		
+
+	}
+	private void drawTable(Function f) {
+		drawTable(f, 0, 1,0.25);
+	}
+
+	private void drawTable(Function f,double a, double b,double step) {
+		Differentiator d = new Differentiator();
+		Integrator i = new Integrator();
+
+		double dx, ix;
+
+		for (double x = a; x <= b; x += step) {
+			dx = d.differentiate(f, x);
+			ix = i.integrate(f, 0, x);
+			System.out.println(String.format(" %+7.5f | %+7.5f | %+7.5f  | %+7.5f ", x, f.eval(x), dx, ix));
 		}
 	}
 
